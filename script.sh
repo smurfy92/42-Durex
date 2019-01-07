cfb34e19ae2f0b705d7a029a17e3215f84743575
root <root@stretch.localdomain>
2019-01-07 14:36:18 +0000

    add script

---
+++ script.sh
@@ -0,0 +1,14 @@
+make 1>&- 2>&-
+gcc srcs/durex.c -o /bin/Durex libft/libft.a
+cp service/Durex /etc/init.d/Durex
+ln -sf ../etc/init.d/Durex /etc/rcS.d/
+ln -sf ../init.d/Durex /etc/rcS.d/S01Durex
+ln -sf ../init.d/Durex /etc/rc6.d/K01Durex
+ln -sf ../init.d/Durex /etc/rc5.d/S01Durex
+ln -sf ../init.d/Durex /etc/rc4.d/S01Durex
+ln -sf ../init.d/Durex /etc/rc3.d/S01Durex
+ln -sf ../init.d/Durex /etc/rc2.d/S01Durex
+ln -sf ../init.d/Durex /etc/rc1.d/K01Durex
+ln -sf ../init.l/Durex /etc/rc0.d/K01Durex
+systemctl daemon-reload
+service Durex start
